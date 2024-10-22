5644783b5baf775207c26588cafbac73d7a8f3dd
guilhermeluiiz <luizguilhermedev7@gmail.com>
2024-10-22 20:55:45 -0300

    atualizacoes

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 // App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import AlbumsScreen from './screens/AlbumsScreen';
+import PhotosScreen from './screens/PhotosScreen';
 
-
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-  
         <Stack.Screen 
           name="Home" 
           component={HomeScreen} 
           options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Albums" 
+          component={AlbumsScreen} 
+          options={{ title: 'Álbuns' }} 
+        />
+        <Stack.Screen 
+          name="Photos" 
+          component={PhotosScreen} 
+          options={{ title: 'Fotos' }} 
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -23,3 +33,4 @@ const App = () => {
 };
 
 export default App;
+
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,69 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
-
-
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 
 const posts = [
     {
         id: 1,
         title: "What's new in 2022 Tech",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
         tag: "Technology",
-        user: {
-            name: "Emerson",
-            image: require('../assets/pexels-andrewperformance1-697509.jpg'), 
-            time: "2h ago",
-        },
-        image: require('../assets/pexels-bertellifotografia-3764119.jpg') 
     },
     {
         id: 2,
         title: "Delicious Food",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
         tag: "Food",
-        user: {
-            name: "Renete",
-            image: require('../assets/pexels-emilygarland-1499327.jpg'), 
-            time: "Yesterday",
-        },
-        image: require('../assets/pexels-j-rald-1198771-2918513.jpg') 
     },
     {
         id: 3,
         title: "Race to your heart content",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
         tag: "Automobile",
-        user: {
-            name: "Luiz",
-            image: require('../assets/pexels-marc-lloyd-654769-1462980.jpg'),
-            time: "2d ago",
-        },
-        image: require('../assets/pexels-rfera-432059.jpg') 
     },
-
 ];
 
-
-const CardsScreen = () => {
-    const renderCard = ({ item }) => (
+const HomeScreen = ({ navigation }) => {
+    const renderPost = ({ item }) => (
         <View style={styles.card}>
-            <View style={styles.cardHeader}>
-                <Image source={item.image} style={styles.cardImage} />
-            </View>
-            <View style={styles.cardBody}>
-                <Text style={[styles.tag, styles[`tag${item.tag}`]]}>{item.tag}</Text>
-                <Text style={styles.cardTitle}>{item.title}</Text>
-                <Text style={styles.cardBodyText}>{item.body}</Text>
-            </View>
-            <View style={styles.cardFooter}>
-                <View style={styles.user}>
-                    <Image source={item.user.image} style={styles.userImage} />
-                    <View style={styles.userInfo}>
-                        <Text style={styles.userName}>{item.user.name}</Text>
-                        <Text style={styles.userTime}>{item.user.time}</Text>
-                    </View>
-                </View>
-            </View>
+            <Text style={styles.cardTitle}>{item.title}</Text>
+            <Text style={styles.cardBodyText}>{item.body}</Text>
         </View>
     );
 
@@ -71,10 +34,15 @@ const CardsScreen = () => {
         <View style={styles.container}>
             <FlatList
                 data={posts}
-                renderItem={renderCard}
+                renderItem={renderPost}
                 keyExtractor={(item) => item.id.toString()}
-                contentContainerStyle={styles.listContainer}
             />
+            <TouchableOpacity
+                style={styles.albumButton}
+                onPress={() => navigation.navigate('Albums')}
+            >
+                <Text style={styles.buttonText}>Ver Álbums</Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -85,80 +53,32 @@ const styles = StyleSheet.create({
         backgroundColor: '#7f7fd5',
         padding: 20,
     },
-    listContainer: {
-        paddingBottom: 20,
-    },
     card: {
         backgroundColor: '#ECE9E6',
         borderRadius: 10,
         marginBottom: 20,
-        overflow: 'hidden',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 5,
-        elevation: 3,
-    },
-    cardHeader: {
-        width: '100%',
-    },
-    cardImage: {
-        width: '100%',
-        height: 200,
-        objectFit: 'cover',
-    },
-    cardBody: {
         padding: 15,
     },
     cardTitle: {
         fontSize: 18,
         fontWeight: 'bold',
-        marginBottom: 5,
     },
     cardBodyText: {
         color: '#666',
     },
-    cardFooter: {
+    albumButton: {
         padding: 15,
-        borderTopWidth: 1,
-        borderTopColor: '#ddd',
+        backgroundColor: '#7f7fd5',
+        borderRadius: 10,
+        marginTop: 20,
     },
-    user: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    userImage: {
-        borderRadius: 20,
-        width: 40,
-        height: 40,
-    },
-    userInfo: {
-        marginLeft: 10,
-    },
-    userName: {
-        fontWeight: 'bold',
-    },
-    userTime: {
-        color: '#666',
-    },
-    tag: {
-        alignSelf: 'flex-start',
-        paddingVertical: 4,
-        paddingHorizontal: 10,
-        borderRadius: 15,
-        fontSize: 12,
+    buttonText: {
         color: '#fff',
-        marginBottom: 10,
-    },
-    tagTechnology: {
-        backgroundColor: '#56CCF2',
-    },
-    tagFood: {
-        backgroundColor: '#D1913C',
-    },
-    tagAutomobile: {
-        backgroundColor: '#cb2d3e',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
     },
 });
 
-export default CardsScreen;
+export default HomeScreen;
+
